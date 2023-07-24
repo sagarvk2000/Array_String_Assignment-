d8291caba2931d461c9f4e7001df32212331d1e7
SnehalKawade <snehkawade@gmail.com>
2023-07-24 18:17:09 +0530

    new program

---
+++ ArrayAssi2/Sechighest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayAssi2
+{
+    public class Sechighest
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 ,4,5};
+
+            int max = arr[0];
+            int max2 = arr[4];
+                       
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] > max)
+                {
+                    max2 = max;
+                    max = arr[i];
+                }
+            }
+            
+            Console.WriteLine(max2);
+        }
+    }
+}
